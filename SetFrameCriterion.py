10495896443fa043b8640894581ad4d66a4d788c
jwn7 <ksyzing@naver.com>
2022-12-12 23:07:20 +0900

    SetFrameCriterion

--- SetFrameCriterion.py
+++ SetFrameCriterion.py
@@ -1,7 +1,10 @@
+import cv2
+
 criteria = ['pass', 'warning', 'stop']
 frame = 0
-status =""
-
+cv2.rectangle()
+# 시작점 좌표(x1,y1) , 종료점 좌표(x2,y2) 절댓값으로 거리차이 계산 => 프레임의 각 변의 길이 알 수 있음
+# 프레임 길이 -> 넓이구하기 -> 넓이가 커질수록 거리가 가까움 -> 거리에 따른 기준 출력
 def frame_if():
     if frame < 50:
         print(criteria[0])
@@ -9,4 +12,6 @@ def frame_if():
         print(criteria[1])
     elif frame < 100:
         print(criteria[2])
+
+
 frame_if()
