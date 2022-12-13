776a6e8a4e964d098b8706f412206bb0021f4609
OssTeam11 <119712312+OssTeam11@users.noreply.github.com>
2022-12-13 21:52:12 +0900

    Delete frame_최승완.py

--- frame_최승완.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-while cv2.waitKey(33) < 0 :
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
